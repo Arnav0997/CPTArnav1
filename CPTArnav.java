14ba6bf832de6bf508c692142045bf65536e189b
Arnav0997 <arnav.arora27@ycdsbk12.ca>
2025-06-02 09:35:19 -0400

    added the first IF statement that switches screen based on what the user selects

--- CPTArnav.java
+++ CPTArnav.java
@@ -4,7 +4,22 @@ public class CPTArnav{
 	public static void main(String[] args){
 		Console con = new Console();
 		
+		int intScreenType;
 		
+		CPTTools.ShowHomescreen(con);
+		intScreenType = con.readInt();
 		
+		if(intScreenType = 1){
+			con.clear(500);
+			CPTTools.ShowPlayscreen(con);
+		}else if(intScreenType = 2){
+			con.clear(500);
+			CPTTools.ShowLeaderboardscreen(con);
+		}else if(intScreenType = 3){
+			con.clear(500);
+			CPTTools.ShowQuitscreen(con); 
+		}else{
+			con.println("That's not an option");
+		}
 	}
 }
