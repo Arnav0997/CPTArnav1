96eeacce7df5ce6c456cfac7bf256bc1544735d2
Arnav0997 <arnav.arora27@ycdsbk12.ca>
2025-06-01 23:12:47 -0400

    Created the homescreen instructions

---
+++ CPTTools.java
@@ -0,0 +1,14 @@
+import arc.*;
+
+public class CPTTools{
+	Console con = new Console();
+		
+		public static void ShowHomescreen(Console con){
+			
+		con.println("Welcome to BlackJack");
+		con.println("Enter 1 to play");
+		con.println("Enter 2 to view leaderboard");
+		con.println("Enter 3 to quit");
+		
+	}
+}
