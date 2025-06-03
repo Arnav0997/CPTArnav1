fb8473d74196f3243dc1e517c6c74cc8cab92e31
Arnav0997 <arnav.arora27@ycdsbk12.ca>
2025-06-03 10:33:25 -0400

    Asks for the users name and their bet

--- CPTArnav.java
+++ CPTArnav.java
@@ -4,19 +4,23 @@ public class CPTArnav{
 	public static void main(String[] args){
 		Console con = new Console();
 		
-		int intScreenType;
+		int intScreenType = 0;
 		
-		CPTTools.ShowHomescreen(con);
-		intScreenType = con.readInt();
+
 		
-		if(intScreenType == 1){
-			CPTTools.ShowPlayscreen(con);
-		}else if(intScreenType == 2){
-			CPTTools.ShowLeaderboardscreen(con);
-		}else if(intScreenType == 3){
-			CPTTools.ShowQuitscreen(con); 
-		}else{
-			con.println("That's not an option");
+		while(intScreenType !=3){			
+			CPTTools.ShowHomescreen(con);
+			intScreenType = con.readInt();
+		
+			if(intScreenType == 1){
+				CPTTools.ShowPlayscreen(con);
+			}else if(intScreenType == 2){
+				CPTTools.ShowLeaderboardscreen(con);
+			}else if(intScreenType == 3){
+				CPTTools.ShowQuitscreen(con); 
+			}else{
+				con.println("That's not an option");
+			}
 		}
 	}
 }
--- CPTTools.java
+++ CPTTools.java
@@ -14,7 +14,24 @@ public class CPTTools{
 	
 	public static void ShowPlayscreen(Console con){
 			
+	String strName;
+	double dblBet = 0;
+	double dblBalance = 1000;
+	
 	con.println("Welcome to BlackJack");
+	con.println("What is your name");
+	strName = con.readLine();
+	con.println("Here is $1000 "+strName+"!");
+	con.println("How much do you want to bet?");
+	dblBet = con.readDouble();
+	
+	if(dblBet > dblBalance || dblBet < 0){
+		con.println("You can't bet that");
+		con.println("enter your new bet");
+		dblBet = con.readDouble();
+		
+		}	
+	
 	
 	}
 	
